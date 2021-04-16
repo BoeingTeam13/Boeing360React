8fdfbe9366217a52bdb085df9bda6581085d70ee
Armando Valdez <avaldez@zagmail.gonzaga.edu>
2021-04-16 14:50:37 -0700

    Map changes

--- src/pages/LiveCam.js
+++ src/pages/LiveCam.js
@@ -11,12 +11,8 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 // });
 
 export default function LiveCamera() {
-  console.log('hello im eric')
   const [isClicked, setClicked] = useState(false);
   const [btnText, setBtnText] = useState('Demo');
-
-  //let module = require('../App.js')
-  //var locations = module.locations;
 
   // var viewer = new Kaleidoscope.Video({
   //   source: 'equirectangular-video.mp4',
@@ -37,88 +33,6 @@ export default function LiveCamera() {
       setBtnText('Demo');
     }
   };
-
-  const locations = [
-    {
-      id: '1',
-      name: 'Hemmingson Center',
-      latlng: [47.6671, -117.3996],
-      description: ['Main activity center for Gonzaga University'],
-      link: ['https://www.gonzaga.edu/about/offices-services/gonzaga-university-event-service-team/venues/john-j-hemmingson-center']
-    },
-    {
-      id: '2',
-      name: 'McCarthy Center',
-      latlng: [47.6653, -117.3991],
-      description: ['Gonzaga University Athletic and Gym Center'],
-      link: ['https://gozags.com/facilities/mccarthey-athletic-center/1']
-    },
-    {
-      id: '3',
-      name: 'Zag Shop',
-      latlng: [47.6677, -117.3977],
-      description: ['Official Gonzaga University Store'],
-      link: ['https://www.bkstr.com/gonzagazagshopstore/home']
-    }
-  ];
-  
-  //experimenting with locations with logs
-  var add_array = []
-  console.log(locations.forEach(function(item, index, array) {
-    var arr = Object.values(item);
-    add_array.push(arr)
-    console.log('name:' + arr[1], index)
-    console.log('description:' + arr[3], index)
-    console.log('link:' + arr[4], index)
-  }));
-  console.log(add_array)
-  const showPOI = () => {
-      console.log('I was triggered during render')
-  };
-
-function generate_table() {
-  // get the reference for the body
-  var body = document.getElementsByTagName("body")[0];
-
-  // creates a <table> element and a <tbody> element
-  var tbl = document.createElement("table");
-  var tblBody = document.createElement("tbody");
-
-  // creating all cells
-  for (var i = 0; i < locations.length; i++) {
-    // creates a table row
-    var row = document.createElement("tr");
-    var poi = locations[i];
-
-    for (var j = 0; j < 3; j++) {
-      // Create a <td> element and a text node, make the text
-      // node the contents of the <td>, and put the <td> at
-      // the end of the table row
-      var cell = document.createElement("td");
-      var cell2 = document.createElement("td");
-      var cellText = document.createTextNode("Name: "+poi.name);
-      var cellText3 = document.createTextNode("\nLink: " + poi.link + "\n")
-      if(j === 1)
-        cell.appendChild(cellText);
-      if(j === 2)
-        cell2.appendChild(cellText3);
-      row.appendChild(cell);
-      row.appendChild(cell2);
-    }
-    var cellText2 = document.createTextNode("\nDescription: "+poi.description)
-    cell.append(cellText2);
-    
-    // add the row to the end of the table body
-    tblBody.appendChild(row);
-  }
-
-  // put the <tbody> in the <table>
-  tbl.appendChild(tblBody);
-  // appends <table> into <body>
-  body.appendChild(tbl);
-  // sets the border attribute of tbl to 2;
-  tbl.setAttribute("border", "2");
-}
 
   // const displayDemo = () => {
   //   var containerSelector = '#container360';
@@ -152,16 +66,15 @@ function generate_table() {
     <div>
       <Container className='LiveCam__container'>
         <h1 style={{ textAlign: 'center' }}>Live-360</h1>
+
         <Row>
           <Col className='col-center'>
             <p>Dynamic POI Table</p>{' '}
-            <Button onClick={generate_table}></Button>
             <table>
               <tbody>
                 <tr>
                   <th>Name:</th>
                   <th>Description:</th>
-                  {/* <th>Link:</th> */}
                 </tr>
               </tbody>
             </table>
--- src/pages/LiveMap.js
+++ src/pages/LiveMap.js
@@ -1,9 +1,21 @@
 import React from 'react';
-import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import { MapContainer, TileLayer, Marker, Popup, Circle} from 'react-leaflet';
 import '../styles/LiveMap.css';
 
 export default function LiveMap() {
+  var lat = 47.667477;
+  var long = -117.409321;
+  const radius = 100
   const position = [47.6670, -117.4014];
+  const circleStart = [lat, long]
+
+  function moveRadius(){
+    if(long > -117.396437){
+      document.getElementById("Error").innerHTML = "You have walked all the way accross campus!!!";
+      return;
+  }
+  long += 0.001000;
+  }
   return (
     <div id='mapid'>
       <MapContainer center={position} zoom={13}>
@@ -11,9 +23,11 @@ export default function LiveMap() {
           attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
           url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
         />
+        {moveRadius()}
+        <Circle center={circleStart} radius={radius}/>
         <Marker position={position}>
           <Popup>
-            Here
+            Circle
           </Popup>
         </Marker>
       </MapContainer>
